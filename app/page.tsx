3fa4705881dfc97718f6ca9164184067c60908da
Reyansh17 <113831002+Reyansh17@users.noreply.github.com>
2025-04-06 18:34:03 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
    <div>
-    <h1 className="text-white text=3xl underline">Home</h1>
+    <h1 className="text-black text-3xl underline">Hello</h1>
    </div>
   );
 }
